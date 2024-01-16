d5be66b8f69a5ebc259cd15551074d68cffdf8db
jashanvirk11 <110488104+jashanvirk11@users.noreply.github.com>
2024-01-17 04:13:45 +0530

    Time: 347 ms (88.06%) | Memory: 14.3 MB (65.55%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        #Initially the final position is the last index
+        last_position  = len(nums)-1
+        # 4 means
+
+        #   If you can reach the final position from this index
+        #    update the final position flag
+# index 3
+        for i in range(len(nums)-2,-1,-1):
+            if (i + nums[i]) >= last_position: 
+               last_position = i 
+
+        return last_position == 0
+
+
+        
