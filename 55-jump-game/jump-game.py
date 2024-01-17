4e0486450934f3405c6a63f691a50e01898b738d
jashanvirk11 <110488104+jashanvirk11@users.noreply.github.com>
2024-01-18 01:28:01 +0530

    Time: 337 ms (98.12%) | Memory: 14.4 MB (65.36%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -7,11 +7,11 @@ class Solution(object):
 
         #Initially the final position is the last index
         last_position  = len(nums)-1
-        # 3means
+        # 4
 
         #   If you can reach the final position from this index
         #    update the final position flag
-# index 3              (3,-1 loop runm -1 decrement)
+#              (4,-1 loop runm -1 decrement)
         for i in range(len(nums)-1,-1,-1):
             # mus[i ]  = jump length 
             if i + nums[i] >= last_position: 
