5ddacc69a623aa091101371468e42984b6aee39b
jashanvirk11 <110488104+jashanvirk11@users.noreply.github.com>
2024-01-22 12:25:44 -0500

    Time: 294 ms (56.64%) | Memory: 64 MB (85.37%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,27 +1,42 @@
-class RandomizedSet(object):
+class RandomizedSet:
 
     def __init__(self):
-        self.list=[]
-        
-        
+        # Initialize the data structures as needed
+        self.value_to_index = {}  # Maps value to its index in the array
+        self.values = []          # Stores the actual values
 
-    def insert(self, val):
-        if val not in self.list:
-            self.list.append(val)
-            return True
-        return False
-        
-        
+    def insert(self, val: int) -> bool:
+        # Check if the value is already present
+        if val in self.value_to_index:
+            return False  # Value already exists, cannot insert
 
-    def remove(self, val):
-        if val in self.list:
-            self.list.remove(val)
-            return True
-        return False
-        
+        # Add the value to the end of the list and update the map
+        self.values.append(val)
+        self.value_to_index[val] = len(self.values) - 1
+        return True  # Successfully inserted
 
-    def getRandom(self):
-        return  random.choice(self.list)
+    def remove(self, val: int) -> bool:
+        # Check if the value is present
+        if val not in self.value_to_index:
+            return False  # Value not found, cannot remove
+
+        # Get the index of the value to be removed
+        index = self.value_to_index[val]
+
+        # Swap the value with the last element in the list
+        self.values[index] = self.values[-1]
+        self.value_to_index[self.values[-1]] = index
+
+        # Remove the last element
+        self.values.pop()
+        del self.value_to_index[val]
+
+        return True  # Successfully removed
+
+    def getRandom(self) -> int:
+        # Return a randomly selected value from the list
+        random_index = random.randint(0, len(self.values) - 1)
+        return self.values[random_index]
         
         
 
